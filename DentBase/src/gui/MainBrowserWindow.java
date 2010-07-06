7004c0156f4df156b5f34f60e5a82284d9281551
benkopolis <benkopolis@d4ac4609-4c2f-a894-aae8-cf2e2b20b33c>
2010-07-06 14:57:56 +0000

    takie tam

---
+++ DentBase/src/gui/MainBrowserWindow.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package gui;
+
+import java.awt.GraphicsConfiguration;
+import java.awt.HeadlessException;
+
+import javax.swing.JFrame;
+
+/**
+ * @author zby
+ *
+ */
+public class MainBrowserWindow extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3532626260218067018L;
+
+	/**
+	 * @throws HeadlessException
+	 */
+	public MainBrowserWindow() throws HeadlessException {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param arg0
+	 */
+	public MainBrowserWindow(GraphicsConfiguration arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param arg0
+	 * @throws HeadlessException
+	 */
+	public MainBrowserWindow(String arg0) throws HeadlessException {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param arg0
+	 * @param arg1
+	 */
+	public MainBrowserWindow(String arg0, GraphicsConfiguration arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+}
